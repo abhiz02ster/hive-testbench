bf5e5053b1953eb3b5ec740a213f7842fa299f97
abhiz02ster <abhiz_02@hotmail.com>
2017-09-06 22:16:38 +0530

    Update tpcds-setup.sh

--- tpcds-setup.sh
+++ tpcds-setup.sh
@@ -2,7 +2,7 @@
 
 function usage {
 	echo "Usage: tpcds-setup.sh scale_factor [temp_directory]"
-	exit 1
+ 	exit 1
 }
 
 function runcommand {
@@ -12,7 +12,7 @@ function runcommand {
 		$1 2>/dev/null
 	fi
 }
-
+ 
 if [ ! -f tpcds-gen/target/tpcds-gen-1.0-SNAPSHOT.jar ]; then
 	echo "Please build the data generator with ./tpcds-build.sh first"
 	exit 1
@@ -22,7 +22,7 @@ if [ $? -ne 0 ]; then
 	echo "Script must be run where Hive is installed"
 	exit 1
 fi
-
+ 
 # Tables in the TPC-DS schema.
 DIMS="date_dim time_dim item customer customer_demographics household_demographics customer_address store promotion warehouse ship_mode reason income_band call_center web_page catalog_page web_site"
 FACTS="store_sales store_returns web_sales web_returns catalog_sales catalog_returns inventory"
@@ -32,7 +32,7 @@ SCALE=$1
 DIR=$2
 if [ "X$BUCKET_DATA" != "X" ]; then
 	BUCKETS=13
-	RETURN_BUCKETS=13
+ 	RETURN_BUCKETS=13
 else
 	BUCKETS=1
 	RETURN_BUCKETS=1
@@ -58,8 +58,7 @@ hdfs dfs -mkdir -p ${DIR}
 hdfs dfs -ls ${DIR}/${SCALE} > /dev/null
 if [ $? -ne 0 ]; then
 	echo "Generating data at scale factor $SCALE."
-	(cd tpcds-gen; hadoop jar target/*.jar -d ${DIR}/${SCALE}/ -s ${SCALE})
-fi
+	(cd tpcds-gen; hadoop jar target/*.jar -d ${DIR}/${SCALE}/ -s ${SCALE})fi
 hdfs dfs -ls ${DIR}/${SCALE} > /dev/null
 if [ $? -ne 0 ]; then
 	echo "Data generation failed, exiting."
@@ -69,7 +68,7 @@ echo "TPC-DS text data generation complete."
 
 # Create the text/flat tables as external tables. These will be later be converted to ORCFile.
 echo "Loading text data into external tables."
-runcommand "hive -i settings/load-flat.sql -f ddl-tpcds/text/alltables.sql -d DB=tpcds_text_${SCALE} -d LOCATION=${DIR}/${SCALE}"
+runcommand "<<BEELINE URL>> -i settings/load-flat.sql -f ddl-tpcds/text/alltables.sql -d DB=tpcds_text_${SCALE} -d LOCATION=${DIR}/${SCALE}"
 
 # Create the partitioned and bucketed tables.
 if [ "X$FORMAT" = "X" ]; then
@@ -81,7 +80,7 @@ DATABASE=tpcds_bin_partitioned_${FORMAT}_${SCALE}
 for t in ${FACTS}
 do
 	echo "Optimizing table $t ($i/$total)."
-	COMMAND="hive -i settings/load-partitioned.sql -f ddl-tpcds/bin_partitioned/${t}.sql \
+	COMMAND="<<BEELINE URL>> -i settings/load-partitioned.sql -f ddl-tpcds/bin_partitioned/${t}.sql \
 	    -d DB=tpcds_bin_partitioned_${FORMAT}_${SCALE} \
 	    -d SOURCE=tpcds_text_${SCALE} -d BUCKETS=${BUCKETS} \
 	    -d RETURN_BUCKETS=${RETURN_BUCKETS} -d FILE=${FORMAT}"
@@ -97,7 +96,7 @@ done
 for t in ${DIMS}
 do
 	echo "Optimizing table $t ($i/$total)."
-	COMMAND="hive -i settings/load-partitioned.sql -f ddl-tpcds/bin_partitioned/${t}.sql \
+	COMMAND="<<BEELINE URL>> -i settings/load-partitioned.sql -f ddl-tpcds/bin_partitioned/${t}.sql \
 	    -d DB=tpcds_bin_partitioned_${FORMAT}_${SCALE} -d SOURCE=tpcds_text_${SCALE} \
 	    -d FILE=${FORMAT}"
 	runcommand "$COMMAND"
